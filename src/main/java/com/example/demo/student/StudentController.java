92cd7c3f0715b3c02806ef77eb587911ba4dedd5
bprado <bastian.prado.v@gmail.com>
2022-05-07 16:18:13 -0700

    StudentService file (service layer) created

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -11,9 +11,14 @@ import java.util.List;
 @RestController //controller class
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
 
     @GetMapping
-    public List<Student> hello(){
-        return List.of(new Student(1L,"Mariam","example@gmail.com", LocalDate.of(2000, Month.JANUARY,5),21));
+    public List<Student> getStudents(){
+        return studentService.getStudents();
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+// service layer
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(new Student(1L,"Mariam","example@gmail.com", LocalDate.of(2000, Month.JANUARY,5),21));
+    }
+}
