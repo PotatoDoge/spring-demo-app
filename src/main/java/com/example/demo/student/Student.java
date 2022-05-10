a58c14395996d5f1b1863929dd3e786a4cbb8459
bprado <bastian.prado.v@gmail.com>
2022-05-10 16:17:07 -0700

    application.properties file now contains the necessary data so that the app connects to the db.

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,13 +1,24 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  this file is the model (architecture related term)
   */
+@Entity //for hibernate
+@Table //for database
 public class Student {
-
-
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
